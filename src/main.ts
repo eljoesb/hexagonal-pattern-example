42fab26039cf0582a55e5abdddee272f49a831ec
joe-kranio <jonathan@kranio.io>
2023-12-07 09:29:57 -0500

    fix import solved

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule.register({ driver: 'orm' }));
+  const app = await NestFactory.create(
+    AppModule.register({ driver: 'in-memory' }),
+  );
   await app.listen(3000);
 }
 bootstrap();
